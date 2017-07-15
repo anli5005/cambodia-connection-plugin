b0fab3f2344683b2c0d1121a5dec750f01411450
anli5005 <li.anthony917@gmail.com>
2017-07-15 12:22:17 -0400

    Add basic plugin header

---
+++ cambodiaconnection.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Cambodia Connection Plugin
+Plugin URI: https://github.com/anli5005/cambodia-connection-plugin
+Description: Manages the homepage and other miscellaneous stuff on the Cambodia Connection website.
+Version: 1.0
+Author: anli5005
+*/
+?>
